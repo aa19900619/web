0af1aabb75139ae1fa6f128d2c74b15da25e0855
jian.tang <jian.tang@molbase.com>
2018-11-20 15:10:15 +0800

    文件访问路径设置

--- src/main/java/com/tangj/web/interceptor/MvcConfiguration.java
+++ src/main/java/com/tangj/web/interceptor/MvcConfiguration.java
@@ -2,16 +2,21 @@ package com.tangj.web.interceptor;
 
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MvcConfiguration extends WebMvcConfigurerAdapter{
 
+	@Value("${file.root}")
+	protected String fileRoot;
+	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		System.out.println("加载静态资源文件路径...........");
 		super.addResourceHandlers(registry);
 		registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+		registry.addResourceHandler("/file/**").addResourceLocations("file:" + fileRoot);
 	}
 
 	
