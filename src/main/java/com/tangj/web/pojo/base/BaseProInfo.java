f26b7cb8bb7ba8bc3c63602418a9d30fcba94c20
aa19900619 <317950455@qq.com>
2018-12-08 15:20:56 +0800

    baseProInfo类

---
+++ src/main/java/com/tangj/web/pojo/base/BaseProInfo.java
@@ -0,0 +1,94 @@
+package com.tangj.web.pojo.base;
+
+public class BaseProInfo extends BaseInfo{
+
+	private Long suppliersId;
+	
+	private String supName;
+	
+	private Long productId;
+	
+	private String productName;
+	
+	private Long goodsSpecifications;
+	
+	private String goodsSpecificationsDesc;
+	
+	private String dicVal;
+	
+	private String goodsCategory;
+	
+	private String goodsCategorys;
+
+	public Long getSuppliersId() {
+		return suppliersId;
+	}
+
+	public void setSuppliersId(Long suppliersId) {
+		this.suppliersId = suppliersId;
+	}
+
+	public String getSupName() {
+		return supName;
+	}
+
+	public void setSupName(String supName) {
+		this.supName = supName;
+	}
+
+	public Long getProductId() {
+		return productId;
+	}
+
+	public void setProductId(Long productId) {
+		this.productId = productId;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public Long getGoodsSpecifications() {
+		return goodsSpecifications;
+	}
+
+	public void setGoodsSpecifications(Long goodsSpecifications) {
+		this.goodsSpecifications = goodsSpecifications;
+	}
+
+	public String getGoodsSpecificationsDesc() {
+		return goodsSpecificationsDesc;
+	}
+
+	public void setGoodsSpecificationsDesc(String goodsSpecificationsDesc) {
+		this.goodsSpecificationsDesc = goodsSpecificationsDesc;
+	}
+
+	public String getDicVal() {
+		return dicVal;
+	}
+
+	public void setDicVal(String dicVal) {
+		this.dicVal = dicVal;
+	}
+
+	public String getGoodsCategory() {
+		return goodsCategory;
+	}
+
+	public void setGoodsCategory(String goodsCategory) {
+		this.goodsCategory = goodsCategory;
+	}
+
+	public String getGoodsCategorys() {
+		return goodsCategorys;
+	}
+
+	public void setGoodsCategorys(String goodsCategorys) {
+		this.goodsCategorys = goodsCategorys;
+	}
+}
