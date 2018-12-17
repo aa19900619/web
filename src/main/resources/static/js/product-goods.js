96d60e2a63952245094ba418951c7600ca8d2956
aa19900619 <317950455@qq.com>
2018-12-17 14:19:55 +0800

    isDoubles，isNum 通用js

---
+++ src/main/resources/static/js/product-goods.js
@@ -0,0 +1,31 @@
+/**
+ * 通用js
+ * 
+ * 
+ * **/
+var bgtype= {"0":"借进","1":"借出"};
+var bgStatustype = {"0":"正常","-1":"已删除","1":"系统对冲"};
+
+function isDoubles(obj) {
+	var val =$(obj).val().trim();
+	val = val.replace(/[^\d.]/g,""); 
+	val = val.replace(/^\./g,""); 
+	val = val.replace(/\.{2,}/g,"."); 
+	val = val.replace(".","$#$").replace(/\./g,"").replace("$#$",".");
+	val = val.replace(/^(\-)*(\d+)\.(\d\d).*$/,'$1$2.$3'); 
+	$(obj).val(val);
+	//$(obj).focus();
+}
+
+function isNum(obj) {
+	var val =$(obj).val().trim();
+	if(isNaN(val)) {
+		val = val.replace(/[^\d.-]/g, '');
+		val = val.replace(/(^\s*)|(\s*$)/g,'');
+		$(obj).val(val);
+		$(obj).focus();
+		return;
+	}
+	$(obj).val(val);
+	return "";
+}
